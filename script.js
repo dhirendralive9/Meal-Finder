340fcbb98ba1ac453be906cc512d78772aa2937e
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-01 10:27:25 +0530

    add search meal function

--- script.js
+++ script.js
@@ -5,37 +5,51 @@ const mealsEl = document.getElementById('meals');
 const resultHeading = document.getElementById('result-heading');
 const single_mealEl = document.getElementById('single-meal');
 
-
+mealsEl.innerHTML = 'hello there';
 
 // Search mmeal and fetch api 
 
-const searchMeal= (e)=>{
+function searchMeal(e) {
     e.preventDefault();
-    
-
-  //clear single meal
-  single_mealEl.innerHTML = ' ';
-
-  //get search term
-  const term = search.value;
   
-  //check for empty
-  if(term.trim()) {
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+  
+    // Get search term
+    const term = search.value;
+  
+    // Check for empty
+    if (term.trim()) {
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then( res => res.json())
-      .then(data => {
-         console.log(data);
-         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
-
-      });
-  } else {
-      alert('please enter a term');
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+  
+          if (data.meals === null) {
+            resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
+          } else {
+            mealsEl.innerHTML = data.meals
+              .map(
+                meal => `
+              <div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data-mealID="${meal.idMeal}">
+                  <h3>${meal.strMeal}</h3>
+                </div>
+              </div>
+            `
+              )
+              .join('');
+          }
+        });
+      // Clear search text
+      search.value = '';
+    } else {
+      alert('Please enter a search term');
+    }
   }
-   
-}
-
-
-
+  
 
 // Event Listeners 
 
